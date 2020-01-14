adefa8f435c97c8be0b631e5f4d6b0ef554df376
Andrei Donets <info@webcube.su>
2020-01-15 00:31:13 +0300

    Заврешил ДЗ 6 задание 6.2 внес правки удалил лишние файлы

--- www/index.php
+++ www/index.php
@@ -4,27 +4,18 @@ require "App/config.php";
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+//Временный файл на будущие если захотим создовать данные из модели
 $capsule = new Capsule;
 $capsule->addConnection([
-    'driver' => 'mysql',
-    'host' => DB_HOST,
-    'port' => DB_PORT,
-    'database' => DB_NAME,
-    'username' => DB_USER,
-    'password' => DB_PASSWORD,
-    'charset' => 'utf8',
-    'collation' => 'utf8_unicode_ci',
+    "driver" => "mysql",
+    "host" => DB_HOST,
+    "port" => DB_PORT,
+    "database" => DB_NAME,
+    "username" => DB_USER,
+    "password" => DB_PASSWORD,
+    "charset" => "utf8",
+    "collation" => "utf8_unicode_ci",
 ]);
 
 $capsule->bootEloquent();
 $capsule->setAsGlobal();
-
-$app = new \Slim\App();
-
-$app->get('/create', function(){
-    $widget = new \MyProject\Model\Widget();
-    $widget->serial_number = 123;
-    $widget->name = 'My Test Widget';
-    $widget->save();
-    echo 'Created!';
-});
